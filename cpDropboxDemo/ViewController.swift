2b77c4b8f5f25602d0fa57ae3d36c22f8b5a2c70
Anthony DeVincenzi <tonydev@gmail.com>
2016-02-02 15:58:53 -0800

    Basic views

---
+++ cpDropboxDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  cpDropboxDemo
+//
+//  Created by Anthony Devincenzi on 2/2/16.
+//  Copyright © 2016 Anthony Devincenzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+class CreateAccountRootViewController : UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        performSegueWithIdentifier("showSignup", sender: self)
+    }
+}
